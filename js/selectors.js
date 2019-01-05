5688ce2354c22d70452a81cf6eb37b11f01c6fc8
BEAU WITKA <bwitka@gmail.com>
2019-01-05 19:42:45 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,21 +1,21 @@
 // declare your functions here...
 
 function paragraphSelector() {
-  
+  $() 
 }
 
 function lastImageSelector() {
-  
+  $()   
 }
 
 function ninjaBabySelector() {
-  
+  $()   
 }
 
 function divSelector() {
-  
+  $()  
 }
 
 function firstListItem() {
-  
+  $()   
 }
